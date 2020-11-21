606f40ad8ce690597a69e63b517697f4cf154528
1021723335@qq.com <1021723335@qq.com>
2020-11-21 11:33:35 +0800

    新建宿管对象，宿管信息盒子，宿管管理对象

---
+++ MboxUI.py
@@ -0,0 +1,149 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from UI._Mbox import Ui_Mbox
+from manager import Manager
+
+class MBox(object):
+    """管理员信息编辑盒 - 基类"""
+
+    def __init__(self):
+
+        self.dialog = QtWidgets.QDialog()
+        window = Ui_Mbox()
+        window.setupUi(self.dialog)
+
+        self.dialog.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
+
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../../../收藏夹/iCloud Photos/Downloads/2019/IMG_0574.PNG"), QtGui.QIcon.Normal,
+                       QtGui.QIcon.Off)
+        self.dialog.setWindowIcon(icon)
+
+        self.MIDEdit= window.MnoEdit        #工号编辑
+        self.MnameEdit = window.MnameEdit  #姓名编辑
+        self.MageEdit = window.MageEdit  #年龄编辑
+        self.MphoneEdit = window.MphonEdit   #楼管电话编辑
+        self.msgLabel = window.msg          #提示信息
+
+        self.okButton = window.okButton
+        self.cancelButton = window.cancelButton
+        self.okButton.clicked.connect(self.onOkButtonClicked)
+        self.cancelButton.clicked.connect(self.dialog.close)
+
+        self.maleButton = window.nanButton
+        self.famaleButton = window.nvButton
+
+    def getSex(self):
+        if self.maleButton.isChecked():
+            return 1
+        elif self.famaleButton.isChecked():
+            return 2
+        else:
+            return 0
+
+    def setSex(self, value):
+        if value == 1:
+            self.maleButton.setChecked(True)
+        elif value == 2:
+            self.famaleButton.setChecked(True)
+
+    def onOkButtonClicked(self):
+        if self.onFinished():
+            self.dialog.close()
+
+    def show(self):
+        self.dialog.show()
+
+    def setTitle(self, title):
+        self.dialog.setWindowTitle(title)
+
+    def setMsg(self, text):
+        self.msgLabel.setText(text)
+
+    def setButton(self, ok, cancel=None):
+        self.okButton.setText(ok)
+        self.cancelButton.setText(cancel) if cancel is not None else None
+
+    def applyToManager(self, manager):
+        manager.MID = self.MIDEdit.text()
+        manager.Mname = self.MnameEdit.text()
+        manager.Msex = self.getSex()
+        manager.Mage = self.MageEdit.text()
+        manager.Mphone = self.MphoneEdit.text()
+
+    def onFinished(self):
+        return False
+
+
+class EditBox(MBox):
+    """编辑宿管信息 - 继承MBox"""
+
+    def __init__(self, manager, callback):
+        super(EditBox, self).__init__()
+        self.callback = callback
+
+        self.setTitle("修改信息...")
+        self.setMsg("")
+        self.setButton("修改")
+
+        self.MIDEdit.setText(manager.MID)
+        self.MnameEdit.setText(manager.Mname)
+        self.setSex(str(manager.Msex))
+        self.MageEdit.setText(manager.Mage)
+        self.MphoneEdit.setText(manager.Mphone)
+
+        self._manager = Manager()
+
+    def onFinished(self):
+        self.applyToManager(self._manager)
+        check, info = self._manager.checkInfo()
+        if check:
+            self.callback(self._manager)
+        else:
+            self.setMsg(info)
+        return check
+
+
+class NewBox(MBox):
+    """新建宿管档案 - 继承MBox"""
+
+    def __init__(self, callback):
+        super(NewBox, self).__init__()
+        self.callback = callback
+
+        self.setTitle("新建档案...")
+        self.setMsg("")
+        self.setButton("新建")
+
+        self.manager = Manager()
+
+    def onFinished(self):
+        manager = self.manager
+        self.applyToManager(manager)
+        check, info = manager.checkInfo(True)
+        self.callback(self.manager) if check else self.setMsg(info)
+        return check
+
+
+class SearchBox(MBox):
+    """高级搜索框 - 继承MBox"""
+
+    def __init__(self, callback):
+        super(SearchBox, self).__init__()
+        self.callback = callback
+
+        self.setTitle("高级搜索...")
+        self.setMsg("请输入检索关键词")
+        self.setButton("搜索")
+
+        self.maleButton.setEnabled(False)
+        self.famaleButton.setEnabled(False)
+
+    def onFinished(self):
+        keyList = [
+            ("SID", ' '.join(self.MIDEdit.text().split())),
+            ("Sname", ' '.join(self.MnameEdit.text().split())),
+            ("Mage", ' '.join(self.MageEdit.text().split())),
+            ("Mphone", ' '.join(self.MphoneEdit.text().split())),
+        ]
+        self.callback(keyList)
+        return True
---
+++ manager.py
@@ -0,0 +1,128 @@
+import sql
+
+#宿管属性集
+attributeListM = [
+    ("MID","工号"),
+    ("Mname","姓名"),
+    ("Msex","性别"),
+    ("Mage","年龄"),
+    ("Mphone","联系方式"),
+]
+class MManager(object):
+    """宿管管理类, 单例"""
+
+    def __init__(self):
+        # 用于存储所有宿管对象
+        self.MList = []
+        # 工号 -> 宿管对象
+        self.MMID = {}
+
+        self.load()
+
+        self.emptyStudent = Manager()
+
+    def add(self, manager):
+        self.MList.append(manager)
+        self.MMID[manager.MID] = manager
+        sql.manager_add(manager)
+
+    def edit(self, manager):
+        self.MMID.pop(manager.MID)
+        self.MList.remove(manager)
+        sql.manager_delete(manager)
+        return True
+
+    def delete(self, manager):
+        self.MMID.pop(manager.MID)
+        self.MList.remove(manager)
+        sql.manager_delete(manager)
+        return True
+
+    def multiSearch(self, keyList):
+        result = self.MList.copy()
+        for searchBy, keyText in keyList:
+            if keyText:
+                result = self.search(searchBy, keyText, result)
+        return result
+
+    def search(self, searchBy, keyList, searchList=None):
+        result = []
+        searchList = searchList or self.MList
+        if not keyList:
+            return searchList.copy()
+        else:
+            keyList = keyList.split()
+            if len(keyList) > 1:
+                [keyList.pop(i) if not i else None for i in keyList]
+            for manager in searchList:
+                target = getattr(manager, searchBy)
+                for key in keyList:
+                    if key in target:
+                        result.append(manager)
+                        break
+            return result
+
+    def load(self):
+        MList = []
+        MMID = {}
+        try:
+            msg = sql.Load("m_table")
+            for i in range(len(msg)):
+                #创建每一个数据的manager对象
+                m = msg[i]
+                #print(s)
+                manager = Manager()
+                manager.MID = m[0]
+                manager.Mname = m[1]
+                manager.Msex = m[2]
+                manager.Mage = m[3]
+                manager.Mphone = m[4]
+                MList.append(manager)
+                MMID[manager.MID] = manager
+            result = True
+        except:
+            result = False
+        finally:
+            self.MList = MList
+            self.MMID = MMID
+
+        print(self.MMID)
+        print(self.MList)
+        return result
+
+class Manager(object):
+    """宿管类, 用于存储宿管基本信息"""
+    def __init__(self, MID="", Mname="", Msex=0, Mage="", Mphone=""):
+        self.MID = MID
+        self.Mname = Mname
+        self.Msex = Msex
+        self.Mage = Mage
+        self.Mphone = Mphone
+
+    def getSex(self):
+        return ["", "男", "女"][self.Msex]
+
+    def copy(self):
+        manager = Manager()
+        self.copyTo(manager)
+        return manager
+
+    def copyTo(self, manager):
+        manager.MID = self.MID
+        manager.Mname = self.Mname
+        manager.Msex = self.Msex
+        manager.Mage = self.Mage
+        manager.Mphone = self.Mphone
+
+    def checkInfo(self,new=False):
+        '''检查自身信息是否完整合法'''
+        # 空值检测
+        for attr, text in attributeListM:
+            if not getattr(self, attr):
+                return (False, "%s不能为空" % text)
+        # 重复性检测
+        check = sql.checkM(self.MID,new)
+        if check[0] ==0:
+            return check
+
+        return (True, "")
